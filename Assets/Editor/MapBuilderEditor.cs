1724b29b70185c388dac795e7fd0cd95d5f7a414
saltire sable <saltiresable@gmail.com>
2015-07-16 19:33:54 -0400

    Map builder now has a button

---
+++ Assets/Editor/MapBuilderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(MapBuilder))]
+public class MapBuilderEditor : Editor
+{
+	public override void OnInspectorGUI ()
+	{
+		DrawDefaultInspector ();
+
+		MapBuilder script = (MapBuilder)target;
+
+		if (GUILayout.Button ("Build Level")) {
+			script.Build ();
+		}
+	}
+}
--- Assets/MapBuilder.cs
+++ Assets/MapBuilder.cs
@@ -9,13 +9,13 @@ public class MapBuilder : MonoBehaviour
 {
 	public TextAsset mapFile;
 
-	public Transform Wall;
-	public Transform Floor;
+	public GameObject Wall;
+	public GameObject Floor;
 
 	static int[] wallChars = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29};
 	static int[] floorChars = {1, 2, 3, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 30};
 
-	void Start ()
+	public void Build ()
 	{
 		XmlDocument xmlDoc = new XmlDocument ();
 		xmlDoc.LoadXml (mapFile.text);
@@ -27,21 +27,36 @@ public class MapBuilder : MonoBehaviour
 		XmlNode dataTag = xmlDoc.GetElementsByTagName ("data") [0];
 		string[] tiles = dataTag.InnerText.Split (new string[] {","}, System.StringSplitOptions.None);
 
+		Transform oldMap = transform.Find ("Map");
+		if (oldMap != null) {
+			DestroyImmediate (oldMap.gameObject);
+		}
+
+		GameObject map = new GameObject ("Map");
+		map.transform.parent = transform;
+
 		for (int i = 0; i < tiles.Length; i++) {
-			int tile = int.Parse (tiles [i]);
+			int tileType = int.Parse (tiles [i]);
+			if (tileType == 0) {
+				continue;
+			}
+
+			tileType -= 1;
+
 			int x = i % width;
 			int z = height - 1 - (i / width);
 
-			if (wallChars.Contains (tile)) {
-				Instantiate (Wall, new Vector3 (x, 0, z), Quaternion.identity);
-			} else if (floorChars.Contains (tile)) {
-				Instantiate (Floor, new Vector3 (x, 0, z), Quaternion.identity);
+			GameObject prefab = null;
+			if (wallChars.Contains (tileType)) {
+				prefab = Wall;
+			} else if (floorChars.Contains (tileType)) {
+				prefab = Floor;
+			}
+
+			if (prefab != null) {
+				GameObject tile = (GameObject)Instantiate (prefab, new Vector3 (x, 0, z), Quaternion.identity);
+				tile.transform.parent = map.transform;
 			}
 		}
-	}
-	
-	void Update ()
-	{
-	
 	}
 }
