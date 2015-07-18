a7291ffe6b7f8274eb2c65079a2019ac1dc24dd2
saltire sable <saltiresable@gmail.com>
2015-07-17 21:34:18 -0400

    Add player movement with 3D physics; add player camera; organize files

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+	public Transform player;
+	public float cameraSpeed = 10f;
+	Vector3 targetCameraPosition;
+	Vector3 cameraDistance;
+	
+	void Start ()
+	{
+		cameraDistance = transform.position - player.position;
+	}
+
+	// Use LateUpdate in case any other effects happen this frame.
+	// Also enable rigidbody interpolation on the player to avoid jitter.
+	void LateUpdate ()
+	{
+		targetCameraPosition = player.position + cameraDistance;
+		transform.position = Vector3.Lerp (transform.position, targetCameraPosition, Time.deltaTime * cameraSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float speed = 10f;
+	public float rotateSpeed = 20f;
+	Rigidbody rb;
+	Quaternion targetRotation;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		targetRotation = transform.rotation;
+	}
+	
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (h != 0f || v != 0f) {
+			rb.AddForce (h * speed, 0f, v * speed);
+
+			targetRotation = Quaternion.LookRotation (new Vector3 (h, 0f, v));
+		}
+		if (transform.rotation != targetRotation) {
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+		}
+	}
+}
