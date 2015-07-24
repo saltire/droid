bf54ae8fb1d39eef64a8811a4fbd673eba422a4c
saltire sable <saltiresable@gmail.com>
2015-07-23 22:57:30 -0400

    Add waypoints to map, use them as random enemy spawn points

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour
+{
+	public GameObject Enemy;
+	public int enemyCount = 1;
+
+	void Start ()
+	{
+		GameObject[] waypoints = GameObject.FindGameObjectsWithTag ("Waypoint");
+		enemyCount = Mathf.Min (enemyCount, waypoints.Length);
+
+		// Spawn enemies on empty waypoints.
+		List<int> placedPoints = new List<int> ();
+		while (placedPoints.Count < enemyCount) {
+			int point = Random.Range (0, waypoints.Length - 1);
+			if (!placedPoints.Contains (point)) {
+				placedPoints.Add (point);
+				Instantiate (Enemy, waypoints [point].transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -8,63 +8,97 @@ using System.Xml;
 public class MapBuilder : MonoBehaviour
 {
 	public TextAsset mapFile;
+	XmlDocument xmlDoc;
+
+	GameObject map;
 
 	public GameObject Door;
 	public GameObject Floor;
 	public GameObject Wall;
+	static int[] doorChars = {2, 3};
+	static int[] floorChars = {4, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 31};
+	static int[] wallChars = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 29, 30};
 
-	static int[] doorChars = {1, 2};
-	static int[] floorChars = {3, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 30};
-	static int[] wallChars = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29};
+	public GameObject Waypoint;
+	
+	int[] getLayerTileData (string layerName)
+	{
+		int[] tilesInt = {};
+		foreach (XmlNode layer in xmlDoc.GetElementsByTagName ("layer")) {
+			if (layer.Attributes ["name"].Value == layerName) {
+				foreach (XmlNode child in layer.ChildNodes) {
+					if (child.Name == "data") {
+						string[] tilesStr = child.InnerText.Split (new string[] {","}, System.StringSplitOptions.None);
+						tilesInt = tilesStr.Select (t => int.Parse (t)).ToArray ();
+					}
+				}
+			}
+		}
+		return tilesInt;
+	}
 
 	public void Build ()
 	{
-		XmlDocument xmlDoc = new XmlDocument ();
+		xmlDoc = new XmlDocument ();
 		xmlDoc.LoadXml (mapFile.text);
 
-		XmlNode levelTag = xmlDoc.GetElementsByTagName ("layer") [0];
-		int width = int.Parse (levelTag.Attributes ["width"].Value);
-		int height = int.Parse (levelTag.Attributes ["height"].Value);
+		XmlNode mapNode = xmlDoc.GetElementsByTagName ("map") [0];
+		int width = int.Parse (mapNode.Attributes ["width"].Value);
+		int height = int.Parse (mapNode.Attributes ["height"].Value);
 
-		XmlNode dataTag = xmlDoc.GetElementsByTagName ("data") [0];
-		string[] tiles = dataTag.InnerText.Split (new string[] {","}, System.StringSplitOptions.None);
+		// Remove any old maps and create a new one.
 
 		Transform oldMap = transform.Find ("Map");
 		if (oldMap != null) {
 			DestroyImmediate (oldMap.gameObject);
 		}
-
-		GameObject map = new GameObject ("Map");
+		map = new GameObject ("Map");
 		map.transform.parent = transform;
 
-		for (int i = 0; i < tiles.Length; i++) {
-			int tileType = int.Parse (tiles [i]);
-			if (tileType == 0) {
-				continue;
-			}
-			tileType -= 1;
+		// Place tiles from the map file.
 
-			int x = i % width;
-			int z = height - 1 - (i / width);
+		int[] tiles = getLayerTileData ("tiles");
+		int[] waypoints = getLayerTileData ("waypoints");
 
-			GameObject prefab = null;
-			if (doorChars.Contains (tileType)) {
-				prefab = Door;
-			} else if (floorChars.Contains (tileType)) {
-				prefab = Floor;
-			} else if (wallChars.Contains (tileType)) {
-				prefab = Wall;
-			}
+		for (int z = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				int i = (height - z - 1) * width + x;
 
-			if (prefab != null) {
-				GameObject tile = (GameObject)Instantiate (prefab, new Vector3 (x, 0, z), Quaternion.identity);
-				tile.transform.parent = map.transform;
-
-				// tile-specific options
-				if (prefab == Door && tileType == 1) {
-					tile.transform.FindChild ("Cylinder").RotateAround (tile.transform.position + new Vector3 (.5f, .5f, .5f), Vector3.up, 90f);
+				if (tiles [i] != 0) {
+					PlaceTile (x, z, tiles [i]);
+				}
+				if (waypoints [i] != 0) {
+					PlaceWaypoint (x, z);
 				}
 			}
 		}
+	}
+
+	void PlaceTile (int x, int z, int tileType)
+	{
+		GameObject prefab = null;
+		if (doorChars.Contains (tileType)) {
+			prefab = Door;
+		} else if (floorChars.Contains (tileType)) {
+			prefab = Floor;
+		} else if (wallChars.Contains (tileType)) {
+			prefab = Wall;
+		}
+		
+		if (prefab != null) {
+			GameObject tile = (GameObject)Instantiate (prefab, new Vector3 (x, 0, z), Quaternion.identity);
+			tile.transform.parent = map.transform;
+			
+			// Tile-specific options
+			if (prefab == Door && tileType == 2) {
+				tile.transform.FindChild ("Cylinder").RotateAround (tile.transform.position + new Vector3 (.5f, .5f, .5f), Vector3.up, 90f);
+			}
+		}
+	}
+
+	void PlaceWaypoint (int x, int z)
+	{
+		GameObject waypoint = (GameObject)Instantiate (Waypoint, new Vector3 (x, 0, z), Quaternion.identity);
+		waypoint.transform.parent = map.transform;
 	}
 }
