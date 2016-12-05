fa42ed8f581790ef0cdbad4810b1c39641aa407f
saltire sable <saltiresable@gmail.com>
2016-12-04 21:09:32 -0500

    Build both sides of minigame; set lights based on input

---
+++ Assets/Scripts/MinigameLight.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MinigameLight : MonoBehaviour {
+	public float flickerSpeed = 20;
+
+	PoweredComponent[] sources = new PoweredComponent[2];
+	Material material;
+
+	void Start() {
+		material = GetComponent<Renderer>().material;
+	}
+
+	public void FindSources() {
+		Vector3 offsetX = new Vector3(transform.localScale.x / 2, 0, 0);
+		Vector3 boxDims = new Vector3(0.5f, transform.localScale.y / 2, 0.5f);
+
+		foreach (Collider other in Physics.OverlapBox(transform.position - offsetX, boxDims)) {
+			PoweredComponent powered = other.GetComponent<PoweredComponent>();
+			if (powered != null) {
+				sources[0] = powered;
+			}
+		}
+		foreach (Collider other in Physics.OverlapBox(transform.position + offsetX, boxDims)) {
+			PoweredComponent powered = other.GetComponent<PoweredComponent>();
+			if (powered != null) {
+				sources[1] = powered;
+			}
+		}
+	}
+
+	void LateUpdate() {
+		bool powered0 = sources[0] != null && sources[0].color != Color.clear;
+		bool powered1 = sources[1] != null && sources[1].color != Color.clear;
+
+		if (powered0 && powered1) {
+			material.SetColor("_EmissionColor", Color.Lerp(sources[0].color, sources[1].color, Mathf.PingPong(Time.unscaledTime * flickerSpeed, 1)));
+		}
+		else if (powered0) {
+			material.SetColor("_EmissionColor", sources[0].color);
+		}
+		else if (powered1) {
+			material.SetColor("_EmissionColor", sources[1].color);
+		}
+		else {
+			material.SetColor("_EmissionColor", Color.clear);
+		}
+	}
+}
---
+++ Assets/Scripts/MinigameSide.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MinigameSide : MonoBehaviour {
+	public Transform source;
+	public Color color = Color.yellow;
+
+	public GameObject WireSegment;
+	public GameObject WireSplitter;
+
+	List<PoweredComponent> startSegments = new List<PoweredComponent>();
+
+	Dictionary<string, GameObject> prefabs;
+
+	class PatternComponent {
+		public string prefab;
+		public int x = 0;
+		public int y = 0;
+		public float xScale = 1;
+	};
+
+	List<List<PatternComponent>> patterns = new List<List<PatternComponent>>() {
+		// Straight
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 4, xScale = 8}
+		},
+		// // Dead end
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 3, xScale = 6}
+		},
+		// Fork
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 1, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 2, xScale = 4},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 2, xScale = 2},
+		},
+		// Fork closer to the end
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 3, y = 1, xScale = 6},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 2, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 2, xScale = 4},
+		},
+		// Fork with a dead end at the top
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 1, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 2, xScale = 4},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 2, xScale = 2},
+		},
+		// Fork with a dead end at the bottom
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 1, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 2, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 2, xScale = 2},
+		},
+		// Reverse fork
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 2, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 1, xScale = 4},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 1, xScale = 2},
+		},
+		// Reverse fork closer to the end
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 3, y = 0, xScale = 6},
+			new PatternComponent {prefab = "WireSegment", x = 3, y = 2, xScale = 6},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 1, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 1, xScale = 4},
+		},
+		// Ring, or a fork followed by a reverse fork
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 1, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 2, xScale = 2},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 1, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 2, xScale = 2},
+		},
+		// Reverse fork followed by a fork
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 2, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 1, xScale = 2},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 0, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 2, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 1, xScale = 2},
+		},
+		// Reverse branching fork
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 2, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 1, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 3, y = 3, xScale = 6},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 2},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 2, xScale = 2},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 1, xScale = 2},
+		},
+		// Reverse fork with a branching fork below it
+		new List<PatternComponent>() {
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 0, xScale = 4},
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 2, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 1},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 1, xScale = 4},
+
+			new PatternComponent {prefab = "WireSegment", x = 2, y = 4, xScale = 4},
+			new PatternComponent {prefab = "WireSplitter", x = 4, y = 4},
+			new PatternComponent {prefab = "WireSegment", x = 5, y = 3, xScale = 2},
+			new PatternComponent {prefab = "WireSplitter", x = 6, y = 3},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 2, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 7, y = 4, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 6, y = 5, xScale = 4},
+
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 1, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 3, xScale = 2},
+			new PatternComponent {prefab = "WireSegment", x = 1, y = 5, xScale = 2},
+		},
+	};
+
+	float offsetX = 19.5f;
+	float offsetY = -26;
+	float rowHeight = -12;
+	float xScale = 15;
+
+	public void Build() {
+		prefabs = new Dictionary<string, GameObject>() {
+			{"WireSegment", WireSegment},
+			{"WireSplitter", WireSplitter}
+		};
+
+		// Place patterns.
+		int row = 0;
+		while (row < 12) {
+			List<PatternComponent> pattern = patterns[Random.Range(0, patterns.Count)];
+			int height = Mathf.Max(pattern.Select(component => component.y).ToArray()) + 1;
+			if (row + height <= 12) {
+				PlacePattern(pattern, row);
+				row += height;
+			}
+		}
+
+		// Set color of power source and (testing) pulsers.
+		source.GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+		foreach (Transform child in transform) {
+			if (child.tag == "Pulser") {
+				child.GetComponent<PoweredComponent>().color = color;
+			}
+		}
+
+		// Build a list of wire segments connected to the power source.
+		foreach (Collider other in Physics.OverlapBox(source.position, new Vector3(source.localScale.x / 2 + 0.5f, source.localScale.y / 2, 0.5f))) {
+			if (other.transform != source && other.tag == "WireSegment") {
+				startSegments.Add(other.GetComponent<PoweredComponent>());
+			}
+		}
+	}
+
+	void Update() {
+		foreach (PoweredComponent startSegment in startSegments) {
+			startSegment.TransmitPower();
+		}
+	}
+
+	void PlacePattern(List<PatternComponent> pattern, int row) {
+		foreach (PatternComponent pc in pattern) {
+			GameObject comp = (GameObject)Instantiate(prefabs[pc.prefab], transform.position, Quaternion.identity);
+			comp.transform.parent = transform;
+			comp.transform.localPosition += new Vector3(pc.x * xScale + offsetX, (pc.y + row) * rowHeight + offsetY, 0);
+			comp.transform.localScale = Vector3.Scale(comp.transform.localScale, new Vector3(pc.xScale, 1, 1));
+		}
+	}
+}
